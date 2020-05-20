1061582d888e018650178b545b5380ebebe27281
unknown <d.andy.emmons@gmail.com>
2020-05-20 17:17:35 -0400

    my first python file

---
+++ main.py
@@ -0,0 +1,16 @@
+print("   / |")
+print("  /  |")
+print(" /   |")
+print("/____|")
+
+
+character_name = "Bill"
+character_age = "100"
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+
+character_name = "Tim"
+character_age = "30"
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + character_age + ".")
