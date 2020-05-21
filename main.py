85d1e28e36cf76bf75587e5d94d98a075bb505d5
unknown <d.andy.emmons@gmail.com>
2020-05-20 22:24:55 -0400

    new update 5/20/20

--- main.py
+++ main.py
@@ -5,12 +5,43 @@ print("/____|")
 
 
 character_name = "Bill"
-character_age = "100"
+character_age = "50"
+is_Male = True
 
 print("There once was a man named " + character_name + ",")
 print("he was " + character_age + " years old.")
 
-character_name = "Tim"
-character_age = "30"
+character_name = "Tom"
+character_age = "34"
 print("He really liked the name " + character_name + ",")
 print("but didn't like being " + character_age + ".")
+
+
+print("Giraffe\nAcademy")
+print("Giraffe\"Academy")
+phrase = "Giraffe Academy"
+
+print(phrase)
+print(phrase[3])
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe", "Elephant"))
+
+print(5 * 6.235 - (8.54 / 5))
+print(10 % 3)
+
+my_num = 5
+print(str(my_num) + " is my favorite numnber")
+
+print(abs(my_num))
+
+my_num = -5
+print(pow(20, 3.3))
+print(max(4, 6))
+
+print(round(3.2))
+
+from math import *
+
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
